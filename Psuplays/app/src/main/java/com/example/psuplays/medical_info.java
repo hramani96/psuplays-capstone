711648818b4f4489cc7ce56fe561caf8f1107832
devghodasara <devghodasara@users.noreply.github.com>
2020-02-23 16:35:16 -0500

    Added medical info page in android

--- Psuplays/app/src/main/java/com/example/psuplays/medical_info.java
+++ Psuplays/app/src/main/java/com/example/psuplays/medical_info.java
@@ -2,24 +2,20 @@ package com.example.psuplays;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class signup extends AppCompatActivity {
+public class medical_info extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_medical_info);
     }
 
-
-    public void button_click(View view) {
-        Intent intent = new Intent(signup.this, medical_info.class);
-        startActivity(intent);
+    public void call_submit(View view) {
+        Toast.makeText(medical_info.this,"Sign Up Successfull",Toast.LENGTH_SHORT).show();
         finish();
     }
 }
