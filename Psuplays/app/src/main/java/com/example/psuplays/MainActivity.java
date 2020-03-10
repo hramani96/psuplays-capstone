0b22daafb819c5e4766834e9aa416dbf5eea5147
Dev Ghodasara <ghodasaradev@gmail.com>
2020-03-10 16:33:41 -0400

    Database connection established in signup page (android-app)

--- Psuplays/app/src/main/java/com/example/psuplays/MainActivity.java
+++ Psuplays/app/src/main/java/com/example/psuplays/MainActivity.java
@@ -8,6 +8,18 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -26,5 +38,66 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("username",(((TextView)findViewById(R.id.etUsername)).getText()).toString());
         startActivity(intent);
     }
-    
+
+    public void tryresponse(String uname,  String Password) throws JSONException {
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http:71.114.127.203:8888/dashboard/";
+        JSONObject form = new JSONObject();
+        form.put("email",uname);
+        form.put("password",Password);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, form, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String status = "";
+                        try {
+                            status = response.getString("status");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        if(status.equals("success")){
+                            Toast.makeText(MainActivity.this,"Signin successfull",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Signin not successfull", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        String responseBody = null;
+                        try {
+                            responseBody = new String(error.networkResponse.data, "utf-8");
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                        JSONObject data = null;
+                        try {
+                            data = new JSONObject(responseBody);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        String message = data.optString("reason");
+                        Toast.makeText(MainActivity.this,message, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+        queue.add(jsonObjectRequest);
+    }
+
+    public void studentDash(View view) throws JSONException {
+        String uname = ((TextView)findViewById(R.id.etUsername)).getText().toString();
+        String password = ((TextView)findViewById(R.id.etPassword)).getText().toString();
+
+        tryresponse(uname,password);
+    }
 }
--- Psuplays/app/src/main/java/com/example/psuplays/signup.java
+++ Psuplays/app/src/main/java/com/example/psuplays/signup.java
@@ -4,9 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 public class signup extends AppCompatActivity {
 
@@ -17,9 +31,75 @@ public class signup extends AppCompatActivity {
     }
 
 
-    public void button_click(View view) {
-        Intent intent = new Intent(signup.this, medical_info.class);
-        startActivity(intent);
-        finish();
+    public void button_click(View view) throws JSONException {
+
+        String email = ((TextView)findViewById(R.id.etEmail)).getText().toString();
+        String fname = ((TextView)findViewById(R.id.etFirstName)).getText().toString();
+        String lname = ((TextView)findViewById(R.id.etLastName)).getText().toString();
+        String password = ((TextView)findViewById(R.id.etPassword)).getText().toString();
+        String cpassword = ((TextView)findViewById(R.id.etRePassword)).getText().toString();
+
+        tryresponse(fname,lname,email,password,cpassword);
+
+    }
+
+    public void tryresponse(String fname, String lname, String email, String Password, String cPassword) throws JSONException {
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http:71.114.127.203:8888/add_student/";
+        JSONObject form = new JSONObject();
+        form.put("first_name",fname);
+        form.put("last_name",lname);
+        form.put("email",email);
+        form.put("password",Password);
+        form.put("password_conf",cPassword);
+
+
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url, form, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String status = "";
+                        try {
+                            status = response.getString("status");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        if(status.equals("success")){
+                            Toast.makeText(signup.this,"Signup successfull",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(signup.this, medical_info.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(signup.this, "Signup not successfull", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        String responseBody = null;
+                        try {
+                            responseBody = new String(error.networkResponse.data, "utf-8");
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                        JSONObject data = null;
+                        try {
+                            data = new JSONObject(responseBody);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        String message = data.optString("reason");
+                        Toast.makeText(signup.this,message, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+        queue.add(jsonObjectRequest);
     }
 }
