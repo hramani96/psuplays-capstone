9eeb14bed8aa5a8415f9cea15975cbc27a1aeb62
Dadzera <57726673+Dadzera@users.noreply.github.com>
2020-03-24 19:14:21 -0400

    Updates urls for Teams

--- webApp/intramural/urls.py
+++ webApp/intramural/urls.py
@@ -14,4 +14,13 @@ urlpatterns = [
     path('admin/getAllAdmins/', views.get_all_admins, name='getAllAdmins'), # Get All Admin api
     #path('student/dashboard/', views.StudentDashboardPageView.as_view(), name='dashboard'), # dashboard url
     #path('adminSports/', views.SportsPageView.as_view(), name='sports'), # sports url
+    path('admin/teamApprove/', views.TeamApprovePageView.as_view(), name='teamApprove'),
+	path('team/create/', views.create_team, name='create_team'), #create team api
+	path('Teams', views.TeamsPageView.as_view(), name='Teams'), #student teams url
+	path('team/getAllTeams/', views.get_all_teams, name='getAllTeams'), #get all teams api
+	path('team/getNewTeams/', views.get_new_teams, name='getNewTeams'), #get new teams api
+	path('team/getApprovedTeams/', views.get_approved_teams, name='getApprovedTeams'), #get all approved teams api
+	path('team/ApproveTeam/', views.approve_team, name='approveTeam'), #approve team api
+	path('team/DenyTeam/', views.deny_team, name='denyTeam'), #deny team api
+	path('admin/teams/', views.AdminTeamsPageView.as_view(), name='Adminteams'), # Admin teams dashboard url
 ]
