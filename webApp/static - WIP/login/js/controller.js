57ceed2dd4a472b4aae7bf73a82e5c8e840fff0d
Dadzera <57726673+Dadzera@users.noreply.github.com>
2020-04-20 22:58:04 -0400

    Delete controller.js

--- webApp/static - WIP/login/js/controller.js
+++
@@ -1,6 +0,0 @@
-angular.module('signup')
-
-.controller('AppCtrl', function() {})
-.controller('UserController', function() {})
-.controller('LoginCtrl', function() {})
-.controller('DashCtrl', function() {});
