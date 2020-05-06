13cd291925ca2ae51e3a0a9846840c7e32c6eab4
Dadzera <57726673+Dadzera@users.noreply.github.com>
2020-05-06 16:46:48 -0400

    Update UserController.js

--- webApp/static/login/js/UserController.js
+++ webApp/static/login/js/UserController.js
@@ -1,11 +1,11 @@
-var app = angular.module('signup', []);
+var app = angular.module('signup', ['ui.router', 'ui.bootstrap']);
 
 app.config(['$httpProvider', function($httpProvider) {
 	$httpProvider.defaults.xsrfCookieName = 'csrftoken';
 	$httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 }]);
 
-app.controller('UserController', function($http, $window, $scope) {
+app.controller('UserController', ['$http', '$window', '$scope', '$rootScope', 'Auth', 'AUTH_EVENTS', function($http, $window, $scope, $rootScope, Auth, AUTH_EVENTS) {
 
 	var vm = this;
 
@@ -70,15 +70,24 @@ app.controller('UserController', function($http, $window, $scope) {
 	}
 
 	vm.login_student = function() {
+		console.log(vm.formInfo)
+		console.log(vm.formInfo.email)
+		
 		$http.post('/loginStudent/', vm.formInfo)
 			.then(function success(response) {
 				console.log(response);
 				toastr.success(" login successful");
+			
+				var loginData = {username: vm.formInfo.email, userRole: "student"};
+				Auth.login(loginData);
+		
+				
 				$window.location.href='/student/dashboard/';
 			},
 				function error(response) {
 					console.log(response);
 					toastr.error("failure : " + response.data.reason);
+					$rootScope.$broadcast(AUTH_EVENTS.loginFailed);
 				});
 	}
 
@@ -87,11 +96,30 @@ app.controller('UserController', function($http, $window, $scope) {
 			.then(function success(response) {
 				console.log(response);
 				toastr.success(" login successful");
+				var loginData = {username: vm.formInfo.email, userRole: "admin"};
+				Auth.login(loginData);
+				
+				$rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
 				$window.location.href='/admin/dashboard/';
 			},
 				function error(response) {
 					console.log(response);
 					toastr.error("failure : " + response.data.reason);
+					$rootScope.$broadcast(AUTH_EVENTS.loginFailed);
 				});
 	}
-});
+	
+	vm.logout = function() {
+		Auth.logout();
+		$window.location.href='/signup/';
+		
+	}
+	
+	if ($window.sessionStorage["userInfo"]) {
+		var credentials = JSON.parse($window.sessionStorage["userInfo"]);
+		console.log("Page refreshed, maintained info: "+$window.sessionStorage["userInfo"]);
+		console.log(credentials);
+		Auth.login(credentials);
+	}
+	
+}]);
